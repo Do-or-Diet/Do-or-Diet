f804327e855515319329a9aa5e06f295a07b1ac0
Brian <bdkeehn@live.com>
2019-10-21 11:00:16 -0500

    restructre for passport

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Requiring necessary npm packages
 var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -10,7 +13,10 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
