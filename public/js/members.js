4e9e215900de758bf20f6e1785a37555b09d6fdf
unknown <sternwillct@gmail.com>
2019-10-23 13:08:08 -0500

    ajax calls are working

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,4 @@
-
+var userDiet="";
 
 $(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
@@ -7,16 +7,29 @@ $(document).ready(function () {
     console.log(data);
     $(".member-name").text(data.email);
     $("#diet").text(data.diet);
-    getRecipe(data.diet);
+    userDiet=data.diet;
   });
-  function getRecipe(diet) {
-    var queryURL = "https://api.edamam.com/search?q=tofu&app_id=edamam-recipe-search.p.rapidapi.com&app_key=5608155f22msh0134e91f7377536p10082bjsn69ced82abbca&health=" + diet;
+  function getRecipe(diet, protein) {
+    var queryURL = "https://api.edamam.com/search?q="+protein+"&app_id=edamam-recipe-search.p.rapidapi.com&app_key=5608155f22msh0134e91f7377536p10082bjsn69ced82abbca&health="+diet;
     $.ajax({
       url: queryURL,
-      method: "GET"
+      method: "GET",
+      datatype: "jsonp"
     }).then(function (response) {
       console.log(response);
     })
   }
+console.log(userDiet);
 
+// function updateDiet(userDiet){
+//   for (let i = 0; i < getRecipe.length; i++) {
+//     var diet = 
+//   }
+$(document).on("click", "#proteinSUBMIT",function(event){
+  event.preventDefault();
+  console.log("we hit dis")
+  var protein = $("#proteinINPUT").val().trim()
+  console.log(protein)
+  getRecipe(userDiet, protein)
+})
 });
